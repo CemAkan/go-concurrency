d2c202618bfb7b68173f39f095e4f9f6c2ccd5a6
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-03 20:49:33 +0300

    init func was wrote for log module

--- 5-select/4-mutexes/log/logger.go
+++ 5-select/4-mutexes/log/logger.go
@@ -1,1 +1,18 @@
 package log
+
+import (
+	"fmt"
+	"os"
+)
+
+var logFile *os.File //global file var
+
+func init() {
+	var err error
+
+	logFile, err = os.OpenFile("bombgame.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println("FATAL ERROR: LOG FILE CAN NOT OPEN/CREATE")
+	}
+}
