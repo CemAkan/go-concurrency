8b99d91a9937799c81108d10eb185cafaaf4e305
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-03 23:48:42 +0300

    adding main menu

--- 5-select/4-mutexes/ui/menu.go
+++ 5-select/4-mutexes/ui/menu.go
@@ -1,1 +1,13 @@
 package ui
+
+import "fmt"
+
+func ShowMainMenu() {
+	fmt.Println("=====================================")
+	fmt.Println("Welcome to THE BOMB GAME v0.1")
+	fmt.Println("=====================================")
+	fmt.Println("Please select a option [1 or 2]:")
+	fmt.Println("1- Host a new game")
+	fmt.Println("2- Join the game")
+	fmt.Println("=====================================")
+}
