d2a76df0cc43d1dfb46db1b16a0ea901fac327b9
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-03 21:22:03 +0300

    global file var was removed, and continued with built-in log lib customization instead of writing a special write a log func

--- 5-select/4-mutexes/log/logger.go
+++ 5-select/4-mutexes/log/logger.go
@@ -2,17 +2,22 @@ package log
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
-var logFile *os.File //global file var
-
 func init() {
-	var err error
 
-	logFile, err = os.OpenFile("bombgame.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	//open if it is exist or create new one
+	logFile, err := os.OpenFile("bombgame.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		fmt.Println("FATAL ERROR: LOG FILE CAN NOT OPEN/CREATE")
 	}
+
+	log.SetOutput(logFile)
+
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	log.Println("Log file initialized")
 }
