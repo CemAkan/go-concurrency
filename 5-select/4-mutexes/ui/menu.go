b2cef2f74f167a3130fef788ba247b950a90b782
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-03 23:54:39 +0300

    making better allocation to main menu's elements

--- 5-select/4-mutexes/ui/menu.go
+++ 5-select/4-mutexes/ui/menu.go
@@ -3,11 +3,11 @@ package ui
 import "fmt"
 
 func ShowMainMenu() {
-	fmt.Println("=====================================")
-	fmt.Println("Welcome to THE BOMB GAME v0.1")
-	fmt.Println("=====================================")
-	fmt.Println("Please select a option [1 or 2]:")
-	fmt.Println("1- Host a new game")
-	fmt.Println("2- Join the game")
-	fmt.Println("=====================================")
+	fmt.Println(" ======================================")
+	fmt.Println("      Welcome to THE BOMBGAME v0.1")
+	fmt.Println(" ======================================\n")
+	fmt.Println("    Please select a option [1 or 2]:\n")
+	fmt.Println("          1- Host a new game")
+	fmt.Println("           2- Join the game")
+	fmt.Println(" ======================================")
 }
