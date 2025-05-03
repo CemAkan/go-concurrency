28ebe0e8eafbf83c4696738adaabfea5fbab1ce4
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-04 00:03:18 +0300

    terminal screen cleaner was added

--- 5-select/4-mutexes/ui/menu.go
+++ 5-select/4-mutexes/ui/menu.go
@@ -10,6 +10,8 @@ var (
 )
 
 func ShowMainMenu() {
+	clearScreen() //clean the entail terminal screen
+
 	fmt.Println("======================================")
 	titleColor.Println("      Welcome to THE BOMBGAME v0.1    ")
 	fmt.Println("======================================\n")
@@ -17,4 +19,8 @@ func ShowMainMenu() {
 	fmt.Println("          1- Host a new game")
 	fmt.Println("           2- Join the game")
 	fmt.Println(" ======================================")
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
 }
