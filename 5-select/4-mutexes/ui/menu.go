ff0d328714b2c41e17a881c88350d59d9c68e390
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-04 00:01:33 +0300

    titleColor(back light cyan, bold and black text) was added to menu

--- 5-select/4-mutexes/ui/menu.go
+++ 5-select/4-mutexes/ui/menu.go
@@ -1,11 +1,18 @@
 package ui
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
+
+var (
+	titleColor = color.New(color.BgHiCyan, color.Bold, color.FgBlack)
+)
 
 func ShowMainMenu() {
-	fmt.Println(" ======================================")
-	fmt.Println("      Welcome to THE BOMBGAME v0.1")
-	fmt.Println(" ======================================\n")
+	fmt.Println("======================================")
+	titleColor.Println("      Welcome to THE BOMBGAME v0.1    ")
+	fmt.Println("======================================\n")
 	fmt.Println("    Please select a option [1 or 2]:\n")
 	fmt.Println("          1- Host a new game")
 	fmt.Println("           2- Join the game")
