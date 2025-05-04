94cc25f8b988546eed2070c96a740094e053d967
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-04 17:15:33 +0300

    bomb struct creator func was added with rand holder selector helper func

--- 5-select/4-mutexes/model/bomb.go
+++ 5-select/4-mutexes/model/bomb.go
@@ -1,10 +1,38 @@
 package model
 
-import "sync"
+import (
+	"math/rand"
+	"sync"
+)
+
+var (
+	hostHolder   = "host"
+	clientHolder = "client"
+	randTimeMin  = 20
+	randTime     = 60
+)
 
 type Bomb struct {
 	mu         sync.Mutex
 	holder     string
 	isExploded bool
 	timeLeft   float64
+}
+
+func NewBomb() *Bomb {
+	return &Bomb{
+		mu:         sync.Mutex{},
+		holder:     randHoldersSelector(),
+		isExploded: false,
+		timeLeft:   float64(rand.Intn(randTime) + randTimeMin),
+	}
+}
+
+func randHoldersSelector() string {
+	randValue := rand.Intn(1) + 1
+
+	if randValue == 1 {
+		return hostHolder
+	}
+	return clientHolder
 }
