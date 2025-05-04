4ddb72964e878cb6bc8d12af516994d7f39b8115
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-04 23:45:35 +0300

    tcp listener with auto port selection was added

--- 5-select/4-mutexes/network/host.go
+++ 5-select/4-mutexes/network/host.go
@@ -1,1 +1,15 @@
 package network
+
+import (
+	"log"
+	"net"
+)
+
+func StartHostTCP() net.Conn {
+	listener, err := net.Listen("tcp", ":0")
+
+	if err != nil {
+		log.Fatal("Tcp port listening error")
+	}
+
+}
