480a3419e6974c97907831a86d3589967429e6a0
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 02:37:35 +0300

    getting ip4 with udp connection with google servers and pushing it with port info to menu (under ui module) for showing on terminal

--- 5-select/4-mutexes/network/host.go
+++ 5-select/4-mutexes/network/host.go
@@ -1,15 +1,28 @@
 package network
 
 import (
+	"bombgame/ui"
 	"log"
 	"net"
 )
 
-func StartHostTCP() net.Conn {
+func StartHostTCP() {
 	listener, err := net.Listen("tcp", ":0")
 
 	if err != nil {
 		log.Fatal("Tcp port listening error")
 	}
+	addr := listener.Addr().(*net.TCPAddr) // getting port and assert the type to interface
 
+	ui.HostInfoShowMenu(getLocalIP(), addr.Port)
+
+}
+
+func getLocalIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8")
+	if err != nil {
+		log.Println("ERROR: UDP connection can not establish with google for getting ip")
+		return "localhost"
+	}
+	return conn.LocalAddr().(*net.TCPAddr).IP.String()
 }
