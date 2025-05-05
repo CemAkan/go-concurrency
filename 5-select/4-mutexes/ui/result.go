6cf29163be0b5458c08d83168430f697c10d138f
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 05:42:46 +0300

    game result terminal shower func was added

--- 5-select/4-mutexes/ui/result.go
+++ 5-select/4-mutexes/ui/result.go
@@ -1,1 +1,27 @@
 package ui
+
+import (
+	"bombgame/conf"
+	"fmt"
+)
+
+func ShowGAmeResult(loserStaus string) {
+
+	var resultText string
+
+	if loserStaus == conf.PlayerStatus {
+		resultText = "YOU LOSE HA HA HA"
+	} else {
+
+		resultText = "WINNER WINNER CHICKEN DINNER"
+	}
+
+	fmt.Println("======================================")
+	fmt.Println("======================================\n")
+
+	fmt.Print(resultText)
+
+	fmt.Println("======================================\n")
+	fmt.Println("======================================")
+
+}
