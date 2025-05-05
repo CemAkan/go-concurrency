98a8cfad54bdd6865a5c7e8ec0b5480c40876320
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 08:37:56 +0300

    gob register was wrote to init

--- 5-select/4-mutexes/main.go
+++ 5-select/4-mutexes/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"bombgame/conf"
 	"bombgame/game"
+	"bombgame/model"
 	"bombgame/network"
 	"bombgame/ui"
+	"encoding/gob"
 )
+
+func init() {
+	gob.Register(&model.Bomb{}) // Mutlaka pointer ile
+}
 
 func main() {
 	conf.LogFileInit()
