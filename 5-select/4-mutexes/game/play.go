0b22033daac3d033dc47b1cc90636e3325a56cf9
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 03:33:32 +0300

    creating encoder & decoder with global conn conf var

--- 5-select/4-mutexes/game/play.go
+++ 5-select/4-mutexes/game/play.go
@@ -3,6 +3,7 @@ package game
 import (
 	"bombgame/conf"
 	_ "bombgame/network" //only init calling
+	"encoding/gob"
 	"log"
 )
 
@@ -10,4 +11,8 @@ import (
 
 func StartGame() {
 	log.Println("Game stated for ", conf.PlayerName)
+
+	//creating encoder & decoder via tcp socket interface (conn)
+	ecn := gob.NewEncoder(conf.GameConn)
+	dec := gob.NewDecoder(conf.GameConn)
 }
