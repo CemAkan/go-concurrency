95737d8f95513db8ae1bccb19d383384ff525888
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 03:02:28 +0300

    HostInfoShowMenu func was added for tcp connection establishing with given address parameters and writing it to global config

--- 5-select/4-mutexes/network/client.go
+++ 5-select/4-mutexes/network/client.go
@@ -1,1 +1,20 @@
 package network
+
+import (
+	"bombgame/conf"
+	"log"
+	"net"
+)
+
+func JoinHostTCP() net.Conn {
+
+	log.Println("Client try to establish with host at ", conf.GameAddress)
+
+	conn, err := net.Dial("tcp", conf.GameAddress)
+	if err != nil {
+		log.Fatalln("I can not establish a tcp connection with host, help me I am a poor client :( ")
+	}
+
+	log.Println("Yeaahhh, client can successfully establish a tcp connection with host at ", conf.GameAddress)
+	return conn
+}
