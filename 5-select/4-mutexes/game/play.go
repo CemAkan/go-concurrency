1f39a9180db0ee3286764deb65681622812c4165
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 08:39:46 +0300

    bombWarper was removed, and deadlock from host in waiting bomb after the first creation was solved

--- 5-select/4-mutexes/game/play.go
+++ 5-select/4-mutexes/game/play.go
@@ -10,55 +10,62 @@ import (
 	"time"
 )
 
-type bombWrapper struct {
-	*model.Bomb
-}
-
 var (
 	enc *gob.Encoder
 	dec *gob.Decoder
 )
 
-func init() {
-	if conf.GameConn == nil {
-		log.Fatal("Global TCP connection is nil (conf.GameConn)")
-	}
-	enc = gob.NewEncoder(conf.GameConn)
-	dec = gob.NewDecoder(conf.GameConn)
-}
-
-// bosss, I swear. I am not play a game, it is a part of my job vallahi diyom
-
 func StartGame() {
 
+	//keyboard opening & closing
 	err := keyboard.Open()
+
 	if err != nil {
-		log.Fatal("Keyboard init error:", err)
+		log.Fatalln("Keyboard open error", err)
 	}
 	defer keyboard.Close()
+
+	//creating encoder & decoder via tcp socket interface (conn)
+	enc = gob.NewEncoder(conf.GameConn)
+	dec = gob.NewDecoder(conf.GameConn)
+
+	//initial conn check
+	if conf.GameConn == nil {
+		log.Fatal("StartGame içinde bağlantı yok (conf.GameConn nil)")
+	}
 
 	log.Println("Game stated for ", conf.PlayerName)
 
 	if conf.PlayerStatus == "host" { //only host one can create a bomb structure because of the solving conflicts (rand holder & time)
-		bomb := &bombWrapper{model.NewBomb()} //initial create
+		bomb := model.NewBomb() //initial create
 		log.Println("Hey, sweety we have a newborn bomb. ", bomb)
 
 		err := enc.Encode(bomb)
 		if err != nil { //sending and error check
 			log.Fatalln("Bomb encoding fatal error")
 		}
-	}
-	for {
-		var bomb bombWrapper // all for loop it will be reset
 
-		if dec.Decode(&bomb) != nil { // receiving bomb and error check
-			ui.ShowWarningMessage("CONNECTION LOST :(")
-			log.Fatalln("I want to a bomb, but you gave me a decoding error whyyy :(")
+	}
+
+	isFirstTurn := true
+
+	for {
+		var bomb model.Bomb
+
+		if !(isFirstTurn && conf.PlayerStatus == "host") {
+			err := dec.Decode(&bomb)
+			if err != nil { // receiving bomb and error check
+				ui.ShowWarningMessage("CONNECTION LOST :(")
+				log.Fatalln("I want to a bomb, but you gave me a decoding error whyyy :( ", err)
+			}
 		}
+		isFirstTurn = false
 
 		ui.ShowTurnInfo(bomb.WhoHold()) // ui module turn info shower on terminal
 
-		bomb.holdSpaceAndDecreaseTime()
+		if bomb.WhoHold() == conf.PlayerStatus {
+			holdSpaceAndDecreaseTime(&bomb)
+		}
 
 		if bomb.IsExploded() {
 			ui.ShowGameResult(bomb.WhoHold())
@@ -67,7 +74,7 @@ func StartGame() {
 	}
 }
 
-func (bomb *bombWrapper) holdSpaceAndDecreaseTime() {
+func holdSpaceAndDecreaseTime(bomb *model.Bomb) {
 	start := time.Now()
 	ticker := time.NewTicker(100 * time.Millisecond)
 	defer ticker.Stop()
