57ad2c583828daea387be893938b0240be5731dc
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 03:32:41 +0300

    say hello to our new little game engine

--- 5-select/4-mutexes/game/play.go
+++ 5-select/4-mutexes/game/play.go
@@ -1,1 +1,13 @@
 package game
+
+import (
+	"bombgame/conf"
+	_ "bombgame/network" //only init calling
+	"log"
+)
+
+// bosss, I swear. I am not play a game, it is a part of my job vallahi diyom
+
+func StartGame() {
+	log.Println("Game stated for ", conf.PlayerName)
+}
