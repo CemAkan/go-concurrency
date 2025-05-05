e4e4cca8b44094af409135e6a00fb3d8b18b68a0
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 05:54:01 +0300

    global sleep time assertion was added

--- 5-select/4-mutexes/main.go
+++ 5-select/4-mutexes/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bombgame/conf"
+	"bombgame/game"
 	"bombgame/ui"
 )
 
 func main() {
 	ui.UserInputStart()
+
+	game.StartGame()
+
+	conf.SleepTime = 4 //global sleep time assertion
 
 }
