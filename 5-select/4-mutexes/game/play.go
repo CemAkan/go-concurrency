0a0d5010e613ae66cb64ef96813f2237f99a35de
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 06:19:58 +0300

    Decoder and encode assertions were moved to init. enc & anc was made global var. Global keyboard opening and closing were added to init

--- 5-select/4-mutexes/game/play.go
+++ 5-select/4-mutexes/game/play.go
@@ -6,20 +6,44 @@ import (
 	_ "bombgame/network" //only init calling
 	"bombgame/ui"
 	"encoding/gob"
+	"github.com/eiannone/keyboard"
 	"log"
 )
+
+type bombWrapper struct{
+	*model.Bomb
+}
+
+var(
+	enc *gob.Encoder
+	dec *gob.Decoder
+)
+
+func init(){
+
+	//global keyboard opening & closing
+	err := keyboard.Open()
+
+	if  err != nil {
+		log.Fatalln("Keyboard open error: %v", err)
+	}
+
+	defer keyboard.Close()
+
+	//creating encoder & decoder via tcp socket interface (conn)
+	enc = gob.NewEncoder(conf.GameConn)
+	dec = gob.NewDecoder(conf.GameConn)
+}
 
 // bosss, I swear. I am not play a game, it is a part of my job vallahi diyom
 
 func StartGame() {
 	log.Println("Game stated for ", conf.PlayerName)
 
-	//creating encoder & decoder via tcp socket interface (conn)
-	enc := gob.NewEncoder(conf.GameConn)
-	dec := gob.NewDecoder(conf.GameConn)
+
 
 	if conf.PlayerStatus == "host" { //only host one can create a bomb structure because of the solving conflicts (rand holder & time)
-		bomb := model.NewBomb() //initial create
+		bomb := &bombWrapper{model.NewBomb()} //initial create
 		log.Println("Hey, sweety we have a newborn bomb. ", bomb)
 
 		if enc.Encode(bomb) != nil { //sending and error check
@@ -27,7 +51,7 @@ func StartGame() {
 		}
 	}
 	for {
-		var bomb *model.Bomb // all for loop it will be reset
+		var bomb bombWrapper // all for loop it will be reset
 
 		if dec.Decode(&bomb) != nil { // receiving bomb and error check
 			ui.ShowWarningMessage("CONNECTION LOST :(")
@@ -36,9 +60,15 @@ func StartGame() {
 
 		ui.ShowTurnInfo(bomb.WhoHold()) // ui module turn info shower on terminal
 
+		bomb.holdSpaceAndDecreaseTime()
+
 		if bomb.IsExploded() {
 			ui.ShowGameResult(bomb.WhoHold())
 			return
 		}
 	}
+}
+
+func (bomb *bombWrapper) holdSpaceAndDecreaseTime{
+
 }
