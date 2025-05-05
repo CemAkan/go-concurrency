e548e9db0bdeb191d2a18b74f265087d87e7c8a2
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 05:18:49 +0300

    ShowTurnInfo (from ui module) call was added

--- 5-select/4-mutexes/game/play.go
+++ 5-select/4-mutexes/game/play.go
@@ -4,6 +4,7 @@ import (
 	"bombgame/conf"
 	"bombgame/model"
 	_ "bombgame/network" //only init calling
+	"bombgame/ui"
 	"encoding/gob"
 	"log"
 )
@@ -17,12 +18,17 @@ func StartGame() {
 	enc := gob.NewEncoder(conf.GameConn)
 	dec := gob.NewDecoder(conf.GameConn)
 
+	var bomb *model.Bomb
+
 	if conf.PlayerStatus == "host" { //only host one can create a bomb structure because of the solving conflicts (rand holder & time)
-		bomb := model.NewBomb()
+		bomb = model.NewBomb()
 		log.Println("Hey, sweety we have a newborn bomb. ", bomb)
 
 		if enc.Encode(bomb) != nil {
 			log.Fatalln("Bomb encoding fatal error")
 		}
 	}
+
+	ui.ShowTurnInfo(bomb.WhoHold())
+
 }
