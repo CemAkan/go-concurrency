0f421a3df2d551249887a8d0627492efb614ecf6
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-05 05:18:13 +0300

    turn message info shower was added

--- 5-select/4-mutexes/ui/turn.go
+++ 5-select/4-mutexes/ui/turn.go
@@ -1,1 +1,20 @@
 package ui
+
+import (
+	"bombgame/conf"
+	"fmt"
+)
+
+func ShowTurnInfo(holderStatus string) {
+	var turnText string
+
+	if holderStatus == conf.PlayerStatus {
+		turnText = ">> It is your turn. Hold space key <<"
+	} else {
+		turnText = ">> It is your friend's turn, wait please <<"
+	}
+
+	fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=")
+	infoTitleColor.Println(turnText)
+	fmt.Println("=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=")
+}
