a05bca46147dc82ac42b187ab6a75336306802b3
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-20 15:32:18 +0300

    feat(handler): Create Fiber HTTP handlers for URL shortening and redirect with click count increment

---
+++ 4-mutexes/internal/handler/handler.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"mutexExercise/internal/db"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func RegisterRoutes(app *fiber.App, database *gorm.DB) {
+	app.Post("/shorten", func(c *fiber.Ctx) error {
+		type Request struct {
+			URL string `json:"url"`
+		}
+
+		var body Request
+		if err := c.BodyParser(&body); err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		shortURL, err := db.CreateShortURL(database, body.URL)
+		if err != nil {
+			return fiber.ErrInternalServerError
+		}
+
+		return c.JSON(fiber.Map{
+			"short_code": shortURL.ShortCode,
+		})
+	})
+
+	app.Get("/:code", func(c *fiber.Ctx) error {
+		code := c.Params("code")
+		shortURL, err := db.GetShortURL(database, code)
+		if err != nil {
+			return fiber.ErrNotFound
+		}
+
+		// Atomically increment click count
+		database.Model(&shortURL).UpdateColumn("click_count", gorm.Expr("click_count + ?", 1))
+
+		return c.Redirect(shortURL.OriginalURL, http.StatusFound)
+	})
+}
