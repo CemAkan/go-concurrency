2f66c411e856eb6910f1773b1aa873a247bb09c6
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-20 16:07:27 +0300

    reflect: rewrite with env getting anf fallback

--- 4-mutexes/internal/config/config.go
+++ 4-mutexes/internal/config/config.go
@@ -1,28 +1,11 @@
 package config
 
-import (
-	"log"
-	"time"
+import "os"
 
-	"github.com/caarlos0/env/v6"
-)
-
-type Config struct {
-	DBHost          string        `env:"DB_HOST" envDefault:"localhost"`
-	DBUser          string        `env:"DB_USER" envDefault:"postgres"`
-	DBPassword      string        `env:"DB_PASSWORD" envDefault:"password"`
-	DBName          string        `env:"DB_NAME" envDefault:"shortener"`
-	DBPort          int           `env:"DB_PORT" envDefault:"5432"`
-	ServerPort      int           `env:"APP_PORT" envDefault:"3000"`
-	Timeout         time.Duration `env:"TIMEOUT" envDefault:"5s"`
-	CleanupInterval time.Duration `env:"CLICK_COUNTER_CLEANUP_INTERVAL" envDefault:"5m"`
-	MaxEntries      int           `env:"CLICK_COUNTER_MAX_ENTRIES" envDefault:"10000"`
-}
-
-func LoadConfig() Config {
-	cfg := Config{}
-	if err := env.Parse(&cfg); err != nil {
-		log.Fatalf("Failed to parse env: %v", err)
+func EnvGet(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
 	}
-	return cfg
+	return val
 }
