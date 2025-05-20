bdd6b0d2d649a87be6baa3ffdad03fa62ffe38c0
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-20 16:20:34 +0300

    feat:add env loader with joho/godotenv external package

--- 4-mutexes/internal/config/config.go
+++ 4-mutexes/internal/config/config.go
@@ -1,11 +1,27 @@
 package config
 
-import "os"
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
-func EnvGet(key, fallback string) string {
-	val := os.Getenv(key)
-	if val == "" {
+func LoadEnv() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("No local .env file found, continue with system env vars.")
+	}
+}
+
+// GetEnv gets env value with key parameter and returning fallback value if env record is not exist
+func GetEnv(key, fallback string) string {
+	value, isExist := os.LookupEnv(key)
+
+	if !isExist {
 		return fallback
 	}
-	return val
+
+	return value
+
 }
