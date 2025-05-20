d60d61bcf8b9efce450c6db6a65261f97e31501d
CemAkan <cemakan@Cems-Mac-mini.local>
2025-05-20 16:28:35 +0300

    fix: atoi add for env-port value

--- 4-mutexes/cmd/server/main.go
+++ 4-mutexes/cmd/server/main.go
@@ -8,17 +8,20 @@ import (
 	"mutexExercise/internal/config"
 	"mutexExercise/internal/db"
 	"mutexExercise/internal/handler"
+	"strconv"
 )
 
 func main() {
 	config.LoadEnv()
+
+	port, _ := strconv.Atoi(config.GetEnv("DBPort", ""))
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
 		config.GetEnv("DBHost", ""),
 		config.GetEnv("DBUser", ""),
 		config.GetEnv("DBPassword", ""),
 		config.GetEnv("DBName", ""),
-		config.GetEnv("DBPort", ""))
+		port)
 
 	database, err := db.Connect(dsn)
 	if err != nil {
@@ -29,6 +32,7 @@ func main() {
 	defer clickCounter.Stop()
 
 	app := fiber.New()
+	app.Listen(":5000")
 
 	handler.RegisterRoutes(app, database, clickCounter)
 }
